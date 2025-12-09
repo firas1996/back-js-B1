2266e3d0c45a599ccaa49d6ca788de765bbfdfbd
firas1996 <firas.hm.1996@gmail.com>
2025-12-09 11:01:18 +0100

    sss

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcryptjs = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -42,6 +43,15 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcryptjs.hash(this.password, 12);
+    this.confirm_password = undefined;
+  }
+  // return next();
+  return next;
 });
 
 const User = mongoose.model("User", userSchema);
